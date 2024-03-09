f5fcf56620929ee14f56dcf0ff9539209123c23e
Freaklord11 <141641366+Freaklord11@users.noreply.github.com>
2024-03-09 10:08:37 +0800

    final

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,30 @@
 
+const startBtn = document.querySelector('[data-start]');
+const stpBtn = document.querySelector('[data-stop]');
+let intervalId;
+
+startBtn.addEventListener('click', startcolorChange);
+stpBtn.addEventListener('click', stopcolorChange);
+stpBtn.disabled=true;
+
+// change color button
+function startcolorChange(){
+    intervalId = setInterval(setRandomColor, 1000);
+    startBtn.disabled= true;
+    stpBtn.disabled= false;
+}
+
+function setRandomColor(){
+    document.body.style.backgroundColor = getRandomHexColor();
+}
+//stop button
+function stopcolorChange(){
+    clearInterval(intervalId);
+    intervalId = null;
+    startBtn.disabled = false;
+
+}
+
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+  }
